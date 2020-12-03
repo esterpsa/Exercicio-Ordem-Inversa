ca4911e610ea79c4dbe1e4b2abd801e3084caa5e
Ester <ester.albuquerque500@gmail.com>
2020-12-03 11:25:12 -0300

    Exercicio Ordem Inersa

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Exercicio_Inverso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int []numeros = new int [15];
+
+            Console.WriteLine("Números");
+            Console.WriteLine("-------------------");
+
+            for(var contador = 0; contador <15; contador++){
+                Console.WriteLine($"Informe o {contador+1}º número");
+                numeros[contador] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("-------------------");
+            Console.WriteLine("Ordem Original");
+        
+            for(var contador = 0; contador < numeros.Length; contador++){
+                Console.WriteLine(numeros[contador]);
+            }
+
+            Array.Reverse(numeros);
+
+            Console.WriteLine("Ordem Inversa");
+            Console.WriteLine("-------------------");
+
+            for(var contador = 0; contador < numeros.Length; contador++){
+                Console.WriteLine(numeros[contador]);
+            }
+        }
+    }
+}
